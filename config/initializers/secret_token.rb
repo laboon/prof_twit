be8f3f9b623a6471a6c687a692372c35a06eff80
Bill Laboon <laboon@gmail.com>
2012-05-17 19:16:19 -0400

    Initial input

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProfTwit::Application.config.secret_token = '9bf2e020eaf74a627fddcd106e03fb8f701a85deef21467f78f6560870ae3021baad3d7db6f7d4ed191d7792296ff62d488a2e0b3d09191c7f9783ad2f954519'

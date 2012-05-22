1b5bd64d276a0be61fe8fd41a2f1b2c609e0e269
Bill Laboon <laboon@gmail.com>
2012-05-22 11:22:15 -0400

    reverting environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ProfTwit::Application.initialize!
-config.gem 'stemmer', :lib => 'lingua/stemmer'
-config.gem 'classifier', :lib => 'classifier'
-config.gem 'twitter'

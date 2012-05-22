54a6ada9de3c20b4700534ac773fc619ea310536
Bill Laboon <laboon@gmail.com>
2012-05-22 11:16:34 -0400

    blarh

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.gem 'stemmer'
-config.gem 'classifier'
-config.gem 'twitter'
-
 # Initialize the rails application
 ProfTwit::Application.initialize!
+config.gem 'stemmer', :lib => 'lingua/stemmer'
+config.gem 'classifier', :lib => 'classifier'
+config.gem 'twitter'
